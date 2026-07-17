aa258989a565767fe842acc9e7b246c21a31a186
Restu Bayu <restu.bayu@mitrakeluarga.com>
2026-07-17 16:49:08 +0700

    feat: setup eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,30 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import { configs, plugins } from 'eslint-config-airbnb-extended';
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      globals: globals.browser,
+    name: 'js/recommended',
+    ...js.configs.recommended,
+  },
+  plugins.stylistic,
+  plugins.importX,
+  ...configs.base.recommended,
+  plugins.react,
+  plugins.reactHooks,
+  plugins.reactA11y,
+  ...configs.react.recommended,
+  plugins.typescriptEslint,
+  ...configs.base.typescript,
+  ...configs.react.typescript,
+  {
+    files: ['**/*.{jsx,tsx}'],
+    rules: {
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
     },
   },
-])
+  reactRefresh.configs.vite,
+]);
